212168bbf79540697a4d5b7bbf711afd33b683c8
Sayantan Ghosh <ghoshsayantan2017@gmail.com>
2023-07-17 00:02:06 +0530

    changes

--- demo/urls.py
+++ demo/urls.py
@@ -3,6 +3,9 @@ from . import views
 from django.conf import settings
  
 urlpatterns = [
-    path('', views.getDemo),
-     path('post/',views.postDemo),
+    path('create/', views.add_items),
+    path('get/',views.view_items),
+    path('item/<int:id>/delete/',views.delete_items),
+    path('update/<int:id>',views.update_items)
+     
 ]
--- demo/views.py
+++ demo/views.py
@@ -3,17 +3,54 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import demo
 from .serializer import demoSerializer
+from rest_framework import status
+from django.shortcuts import get_list_or_404, get_object_or_404
  
 # Create your views here.
-@api_view(['GET'])
-def getDemo(request):
-    demodetails = demo.objects.all()
-    serializer = demoSerializer(demodetails, many=True)
-    return Response(serializer.data)
-
 @api_view(['POST'])
-def postDemo(request):
-    serializer = demoSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
+def add_items(request):
+    item = demoSerializer(data=request.data)
+ 
+    # validating for already existing data
+    if demo.objects.filter(**request.data).exists():
+        raise demoSerializer.ValidationError('This data already exists')
+ 
+    if item.is_valid():
+        item.save()
+        return Response(item.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['GET'])
+def view_items(request):
+     
+     
+    # checking for the parameters from the URL
+    if request.query_params:
+        items = demo.objects.filter(**request.query_params.dict())
+    else:
+        items = demo.objects.all()
+ 
+    # if there is something in items else raise error
+    if items:
+        serializer = demoSerializer(items, many=True)
+        return Response(serializer.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+@api_view(['PUT'])
+def update_items(request, id):
+    item = demo.objects.get(id=id)
+    data = demoSerializer(instance=item, data=request.data)
+ 
+    if data.is_valid():
+        data.save()
+        return Response(data.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+@api_view(['DELETE'])
+def delete_items(request, pk):
+    item = get_object_or_404(item, pk=pk)
+    item.delete()
+    return Response(status=status.HTTP_202_ACCEPTED)
